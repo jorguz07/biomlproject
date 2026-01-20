f1f57b359c6b798df0a9d1d30ef4a63ed92093a9
jorguz07 <jorguzjma07@hotmail.com>
2026-01-20 14:25:00 +0100

    annotations logger and exception files

--- src/exception.py
+++ src/exception.py
@@ -1,21 +1,34 @@
+#we create a custom exception we want raised whenever an error happens. It summarizes error info
+#in an easier to read way
+ 
 import sys
-from logger import logging #generate a log for the script in this file
+from logger import logging #we want errors to be logged too!
 
-#we create a function that takes an error from the system and transaltes is into something easier
-#for us to read, as a string
+#we create a function that takes an error from the system and transaltes it
 def error_message_detail(error, error_detail:sys):
-    _,_,exc_tb=error_detail.exc_info()
-    file_name=exc_tb.tb_frame.f_code.co_filename
+    '''Takes error object and its details from system and creates a string that summarizes it
+    and makes it easier to read. Prints file that generated the error, line and system error msg'''
+    _,_,exc_tb=error_detail.exc_info() #extract tb object
+    file_name=exc_tb.tb_frame.f_code.co_filename #get file name from tb obj
+    error_line=exc_tb.tb_lineno #get line from tb obj
     error_message="Error occured in python script [{0}] line number[{1}] error message[{2}]".format(
-        file_name,exc_tb.tb_lineno,str(error))
+        file_name,error_line,str(error))
 
     return error_message
+#sys.exec_info() returns a tuple (tuple, value, traceback) e.g. (ValueError, ValueError("bad input), <traceback object>")
+#_ means we ignore the first two, we just store the traceback obj as exc_tb
 
-#we create a class for the error message
-class CustomException(Exception):
-    def __init__(self, error_message, error_detail:sys):
+#getting info from tb obj
+#exc_tb.tb_frame.f_code.co_filename (somehow) extracts the name of the file that generated the error
+#exc_tb.tb_lineno extracts line of code that generated error
+
+
+#we create a custom exception that uses our error_message_detail function
+class CustomException(Exception): #new class CustomException is an Exception
+    def __init__(self, error_message, error_detail:sys): #def
+        logging.error(self.error_message) #chatgpt recommendation
         super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
 
-    def __str__(self):
+    def __str__(self): #method
         return self.error_message
--- src/logger.py
+++ src/logger.py
@@ -1,24 +1,31 @@
 #we want to keep a record of things going right or wrong, particularly where, why, with which
-#inputs, a problem was caused
+#inputs
 
-import os
+import os #work with paths and dirs, code works in any OS
 import logging #python default logging system
 from datetime import datetime
 
-#every run gets a log file
+#NAMING LOG FILE
+#we want a unique file per run, we use date and time to create unique names
 LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+#datetime.now() returns current date and time
+#.strftime() converts date and time data to a string of specific format
 
-#where to store log file
-logs_dir = os.path.join(os.getcwd(), "logs")
-os.makedirs(logs_dir, exist_ok=True)
-LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)
+#STORING LOG FILE
+logs_dir = os.path.join(os.getcwd(), "logs") #path to logs folder
+os.makedirs(logs_dir, exist_ok=True) #craetes logs folder based on path
+LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE) #path to log file
+#os.getcwd() returns string of current path
+#os.path.joins() joins strings
 
-#log file structure
+#CREATING LOG FILE
+#we import this function to toher scripts in src right after a piece of code we want to evaluate,
+#we add a custom message on each use
 logging.basicConfig(
-    filename=LOG_FILE_PATH,
+    filename=LOG_FILE_PATH, #notice, whole path! w/o this logs go to terminal
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
+    #date and time, line, level (in this case, info, warning, error, critical), message (customizable)
+    level=logging.INFO, #what we store, INFO stores (INFO, WARNING, ERROR, CRITICAL), there are other 'levels'
 )
-#we will import this function in other files in src and specifify a message, the idea is to 
-#generate a log file for the script in said file, with a message of our choice
+
     
