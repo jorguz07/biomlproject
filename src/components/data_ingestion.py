d2fcdaebe6bfeea05c5e26fb2f944c4a5cd13133
jorguz07 <jorguzjma07@hotmail.com>
2026-03-20 17:52:54 +0100

    first draft model_trainer_py

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -17,6 +17,9 @@ from dataclasses import dataclass
 from src.components.data_transform import DataTransformation
 from src.components.data_transform import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 #class to hold vars, to be used in data ingestion
 @dataclass #to avoid __init__ (mostly only for data containers!)
 class DataIngestionConfig:
@@ -99,7 +102,12 @@ if __name__=='__main__':
     train_data, test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation( train_data, test_data )
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation( train_data, test_data )
+
+    modeltrainer=ModelTrainer()
+    print( modeltrainer.initiate_model_trainer( train_arr, test_arr ) )
+
+
 #every python files has a built in varaible __name__. If file ran directly, __name__ = '__main__', so the
 #block runs. If the file is imported, __name__ ='src.components.data_ingestion' and the block doesn't run
 
